bfce9c5a181cdaee0434a89313ca05b956929eb3
Matilde Fortunato <matilde84@gmail.com>
2019-08-15 15:38:01 -0300

    add 404 page

--- js/plan-renault.js
+++ js/plan-renault.js
@@ -47,19 +47,16 @@ $(document).ready(function () {
       function updateGallery(selector) {
         let $sel = selector;
         current_image = $sel.data('image-id');
-        $('#image-gallery-image')
-          .attr('src', $sel.data('image'));
+        $('#image-gallery-image').attr('src', $sel.data('image'));
         	disableButtons(counter, $sel.data('image-id'));
       }
       if (setIDs == true) {
-        $('[data-image-id]')
-          .each(function () {
+        $('[data-image-id]').each(function () {
             counter++;
             $(this).attr('data-image-id', counter);
           });
       }
-      $(setClickAttr)
-        .on('click', function () {
+      $(setClickAttr).on('click', function () {
           updateGallery($(this));
         });
     }
